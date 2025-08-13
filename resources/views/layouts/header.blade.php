0bc2c9905f6b57ae0a3d06c054f10943ff7f9ee7
projectlifed <projectlifed@gmail.com>
2025-08-13 11:21:02 +0300

    changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,15 +5,8 @@
 </div>
 <div class="main-header">
     <img src="{{ asset('images/lifed-logo-black.webp') }}" alt="Lifed Logo" class="logo">
-    <!-- <nav>
-            <a href="#">Home</a>
-            <a href="#">Wellness Products</a>
-            <a href="#">About Us</a>
-            <a href="#">Add Your Program</a>
-            <a href="#">Contact Us</a>
-        </nav> -->
-
     <div class="right">
+        <a href="#" class="support-link"><span class="icon">📞</span> Quick Support</a>
         @auth
             <a href="#" class="logout-link"
                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
@@ -27,7 +20,5 @@
         @guest
             <a href="{{ route('login') }}" class="signin-link"><span class="icon">🔑</span> SignIn</a>
         @endguest
-
-        <a href="#" class="support-link"><span class="icon">📞</span> Quick Support</a>
     </div>
 </div>
